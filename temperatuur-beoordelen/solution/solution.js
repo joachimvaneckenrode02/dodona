17dfc5add23cc92c5c78981fc2dd27f8ac4dc4c6
joachimvaneckenrode02 <joachim.vaneckenrode02@gmail.com>
2026-05-05 09:33:22 +0200

    Update solution.js

--- temperatuur-beoordelen/solution/solution.js
+++ temperatuur-beoordelen/solution/solution.js
@@ -1,1 +1,11 @@
-
+function beoordeelTemperatuur(temperatuur) {
+  if (temperatuur < 0) {
+    return "Het vriest";
+  } else if (temperatuur <= 15) {
+    return "Het is koud";
+  } else if (temperatuur <= 25) {
+    return "Het is aangenaam";
+  } else {
+    return "Het is warm";
+  }
+}
