b4d0dadaca7f79dcb7a1e7def2792c77823a02b4
joachimvaneckenrode02 <joachim.vaneckenrode02@gmail.com>
2026-05-14 21:27:54 +0200

    Voeg oplossing toe voor even-oneven-test

---
+++ even-oneven-test/solution/solution.js
@@ -0,0 +1,7 @@
+var getal = Number(prompt("Geef een getal in:"));
+
+if (getal % 2 == 0) {
+  console.log("Het getal " + getal + " is even.");
+} else {
+  console.log("Het getal " + getal + " is oneven.");
+}
