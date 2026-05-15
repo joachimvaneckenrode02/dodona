29c7b13966ee3e44a2df75908ac2b9f5bee2a025
joachimvaneckenrode02 <joachim.vaneckenrode02@gmail.com>
2026-05-15 12:32:30 +0200

    Herwerk H1 schoolmateriaal basis oplossing

--- h1-sequentie-schoolmateriaal-basis/solution/solution.py
+++ h1-sequentie-schoolmateriaal-basis/solution/solution.py
@@ -1,12 +1,23 @@
+naam = input()
 aantal_schriften = int(input())
-prijs_schrift = int(input())
-aantal_pennen = int(input())
-prijs_pen = int(input())
+prijs_schrift = float(input())
+aantal_potloden = int(input())
+potloden_per_doos = int(input())
+prijs_doos = float(input())
 
 kost_schriften = aantal_schriften * prijs_schrift
-kost_pennen = aantal_pennen * prijs_pen
-totaal = kost_schriften + kost_pennen
+aantal_dozen = aantal_potloden // potloden_per_doos + 1
+over = aantal_dozen * potloden_per_doos - aantal_potloden
+kost_potloden = aantal_dozen * prijs_doos
+totaal = kost_schriften + kost_potloden
 
+kost_schriften = round(kost_schriften, 2)
+kost_potloden = round(kost_potloden, 2)
+totaal = round(totaal, 2)
+
+print("Bestelling voor " + naam + ".")
 print("De schriften kosten", kost_schriften, "euro.")
-print("De pennen kosten", kost_pennen, "euro.")
+print("Je koopt", aantal_dozen, "dozen potloden.")
+print("Er zijn", over, "potloden over.")
+print("De potloden kosten", kost_potloden, "euro.")
 print("In totaal betaal je", totaal, "euro.")
