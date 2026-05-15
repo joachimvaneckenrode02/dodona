9d86d83e2ee51d906f3f20cf389585be490449c3
joachimvaneckenrode02 <joachim.vaneckenrode02@gmail.com>
2026-05-15 11:10:54 +0200

    Voeg H1 Python oefening schoolmateriaal basis toe

---
+++ h1-sequentie-schoolmateriaal-basis/solution/solution.py
@@ -0,0 +1,12 @@
+aantal_schriften = 4
+prijs_schrift = 3
+aantal_pennen = 6
+prijs_pen = 2
+
+kost_schriften = aantal_schriften * prijs_schrift
+kost_pennen = aantal_pennen * prijs_pen
+totaal = kost_schriften + kost_pennen
+
+print("De schriften kosten", kost_schriften, "euro.")
+print("De pennen kosten", kost_pennen, "euro.")
+print("In totaal betaal je", totaal, "euro.")
